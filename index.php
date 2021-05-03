4a7e735fcccd91defb2c43a785335ab0a657cf24
Matia <matia.casu@gmail.com>
2021-05-03 18:10:42 +0200

    Aggiunte concatenazioni

--- index.php
+++ index.php
@@ -13,12 +13,11 @@
     <h2>Frase originale</h2>
     <p><?php echo $frase; ?></p>
     <h2>Lunghezza</h2>
-    <p><?php echo strlen($frase); ?></p>
-    <h2>Parolaccia</h2>
-    <p><?php echo $parolaccia; ?></p>
+    <p><?php echo 'La lunghezza della frase è: '.strlen($frase); ?></p>
     <?php if(strpos($frase, $parolaccia)): ?>
       <h2>Frase censurata</h2>
       <p><?php echo str_replace($parolaccia, "***", $frase); ?></p>
+      <p><?php echo "Parolaccia: $parolaccia"; ?></p>
     <?php endif; ?>
   </body>
 </html>
